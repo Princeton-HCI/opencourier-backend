7ff6f879cc3e2b7f1f9e61012f5bc08297774349
Wilmer Cantillo <dev@wilmercantillo.com>
2024-09-16 14:42:52 -0500

    feat: check for driver availability when generating a quote.

--- src/rest-api/delivery-quote/partner/delivery-quote.partner.rest-api.module.ts
+++ src/rest-api/delivery-quote/partner/delivery-quote.partner.rest-api.module.ts
@@ -4,11 +4,12 @@ import { DeliveryQuoteDomainModule } from 'src/domains/delivery-quote/delivery-q
 import { PartnerDomainModule } from 'src/domains/partner/partner.domain.module'
 import { DeliveryQuotePartnerRestApiService } from './delivery-quote.partner.rest-api.service'
 import { LocationDomainModule } from 'src/domains/location/location.domain.module'
+import { CourierMatcherModule } from 'src/services/courier-matcher/courier-matcher.module'
 
 @Module({
   providers: [DeliveryQuotePartnerRestApiService],
   exports: [DeliveryQuotePartnerRestApiService],
-  imports: [DeliveryQuoteDomainModule, PartnerDomainModule, LocationDomainModule],
+  imports: [DeliveryQuoteDomainModule, PartnerDomainModule, LocationDomainModule, CourierMatcherModule.forRoot()],
   controllers: [DeliveryQuotePartnerRestApiController],
 })
 export class DeliveryQuotePartnerRestApiModule {}
